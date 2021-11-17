db43435ab836e9c951c263817d06f25ec9cca59c
Ekoterina <solomennicova555@gmail.com>
2021-11-18 00:53:21 +0200

    Edit player music const-sett

--- src/main/java/MusicalPlayer.java
+++ src/main/java/MusicalPlayer.java
@@ -1,7 +1,14 @@
 public class MusicalPlayer {
     private  Music music;
+
     public MusicalPlayer(Music music){
         this.music=music;
+    }
+
+    public MusicalPlayer() {}
+
+    public void setMusic(Music music) {
+        this.music = music;
     }
 
     public void playMusic(){
